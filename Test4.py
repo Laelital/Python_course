5dfb3141beff010c2240e3ee19018ffb6237ad97
Laelital <56368810+Laelital@users.noreply.github.com>
2025-01-16 22:56:03 +0300

    Update Test4.py

--- Test4.py
+++ Test4.py
@@ -1,14 +1,6 @@
 # Напишите метод, который будет вычислять угол между часовыми и минутными стрелками часов. На вход функции подаётся время в виде двух переменных: "hours" и "minutes".
 # Расчет угла производить относительно реального поведения стрелок часов.
 
-# def method_corner1(hours: int, minutes: int):
-#     if 0 <= hours <=11 and 0 <= minutes <= 60:
-#         return minutes*6 - (hours + minutes/60) * 30
-#     else:
-#         return None
-
-# print(method_corner1(0, 59))
-
 def method_corner(hours: int, minutes: int):
     if 0 <= hours <=11 and 0 <= minutes <= 60:
         corner_m = minutes*6
